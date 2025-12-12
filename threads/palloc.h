b3002f01c0353607e6aedd2f046723adc8239aa2
xodyd051127-star <xodyd051127@gmail.com>
2025-12-12 20:16:46 +0900

    Update palloc.h

--- threads/palloc.h
+++ threads/palloc.h
@@ -11,6 +11,16 @@ enum palloc_flags
     PAL_USER = 004    /* User page. */
 };
 
+enum palloc_mode
+{
+    PAL_FIRST_FIT, // 기본 제공
+    PAL_NEXT_FIT,  // Next Fit 할당 기법
+    PAL_BEST_FIT,  // Best Fit 할당 기법
+    PAL_BUDDY      // Buddy System 할당 기법
+};
+
+void palloc_set_mode (enum palloc_mode);
+
 void palloc_init (size_t user_page_limit);
 void *palloc_get_page (enum palloc_flags);
 void *palloc_get_multiple (enum palloc_flags, size_t page_cnt);
